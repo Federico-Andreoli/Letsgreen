1ca39bbb9445fa58bdd41536ebbb732b104222ef
Lorenzo Basavecchia <l.basavecchia@campus.unimib.it>
2021-07-28 18:09:58 +0200

    modifiche fragment_dashboard.xml

--- app/src/main/java/it/unimib/lets_green/MainActivity.java
+++ app/src/main/java/it/unimib/lets_green/MainActivity.java
@@ -1,13 +1,16 @@
 package it.unimib.lets_green;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -30,6 +34,7 @@ import it.unimib.lets_green.ui.notifications.NotificationsFragment;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,31 +56,6 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
 
-        /* inserire la selezione del fragment in base al bottone premuto
-
-        if(savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(R.id.scene_root, new HomeFragment()).commit();
-        }
-        */
-
-       /* NavHostFragment navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        NavigationUI.setupWithNavController(bottomNav, navController);
-
-        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
-            @Override
-            public void onDestinationChanged(@NonNull NavController controller,
-                                             @NonNull NavDestination destination, @Nullable Bundle arguments) {
-                if(destination.getId() == R.id.full_screen_destination) {
-                    toolbar.setVisibility(View.GONE);
-                    bottomNavigationView.setVisibility(View.GONE);
-                } else {
-                    toolbar.setVisibility(View.VISIBLE);
-                    bottomNavigationView.setVisibility(View.VISIBLE);
-                }
-            }
-        });*/
     }
 
     @Override
@@ -84,37 +64,5 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.top_bar_menu, menu);
         return true;
     }
-
-    /*
-    private final BottomNavigationView.OnNavigationItemSelectedListener listener= new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected( MenuItem item) {
-            Fragment selected_fragment =null;
-            switch (item.getItemId()){
-
-                case R.id.:
-                    selected_fragment =HomeFragment.newInstance();
-                    break;
-
-                case R.id.:
-                    selected_fragment = DashboardFragment.newInstance();
-                    break;
-
-                case R.id.settings:
-                    selected_fragment = NotificationsFragment.newInstance();
-                    break;
-            }
-            if (selected_fragment != null) {
-                FragmentManager fm= getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.scene_root, selected_fragment);
-                ft.addToBackStack("attività");
-                ft.commit();
-
-            }
-            return true;
-        }
-    };
-*/
 
 }
--- app/src/main/java/it/unimib/lets_green/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/it/unimib/lets_green/ui/dashboard/DashboardFragment.java
@@ -1,28 +1,82 @@
 package it.unimib.lets_green.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.jetbrains.annotations.NotNull;
+
+import it.unimib.lets_green.Cat1Fragment;
+import it.unimib.lets_green.Cat2Fragment;
+import it.unimib.lets_green.Cat3Fragment;
+import it.unimib.lets_green.MainActivity;
 import it.unimib.lets_green.R;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+
+    private Cat1Fragment cat1Fragment;
+    private Cat2Fragment cat2Fragment;
+    private Cat3Fragment cat3Fragment;
+
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        startActivity(new Intent(getActivity(),MainActivity.class));
+
+        cat1Fragment = new Cat1Fragment();
+        cat2Fragment = new Cat2Fragment();
+        cat3Fragment = new Cat3Fragment();
+
+        viewPager = ((MainActivity)getActivity()).findViewById(R.id.view_pager);
+        tabLayout = ((MainActivity)getActivity()).findViewById(R.id.tabs);
+
+        viewPager.setAdapter(new ViewPagerAdapter(this));
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("cat1");
+                    break;
+                case 1:
+                    tab.setText("cat2");
+                    break;
+                case 2:
+                    tab.setText("cat3");
+                    break;
+            }
+        }).attach();
+
+        //tabLayout.setupWithViewPager(viewPager);
+
+        //ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(((MainActivity)getActivity()).getSupportFragmentManager());
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -31,5 +85,33 @@ public class DashboardFragment extends Fragment {
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         return root;
+    }
+
+    private class ViewPagerAdapter extends FragmentStateAdapter {
+
+        public ViewPagerAdapter(@NonNull @NotNull DashboardFragment dashboardFragment) {
+            super(dashboardFragment);
+        }
+
+        @NonNull
+        @NotNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new Cat1Fragment();
+                case 1:
+                    return new Cat2Fragment();
+                case 2:
+                    return new Cat3Fragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
     }
 }
