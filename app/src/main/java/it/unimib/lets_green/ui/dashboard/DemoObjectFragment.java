cbc48873398939635710fa7af629a3a4f0281686
Lorenzo Basavecchia <l.basavecchia@campus.unimib.it>
2021-07-29 17:40:44 +0200

    finita implementazione scroll tra tabs

--- app/src/main/java/it/unimib/lets_green/ui/dashboard/DemoObjectFragment.java
+++ app/src/main/java/it/unimib/lets_green/ui/dashboard/DemoObjectFragment.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import it.unimib.lets_green.R;
 
 // Instances of this class are fragments representing a single
 // object in our collection.
 public class DemoObjectFragment extends Fragment {
-    public static final String ARG_OBJECT = "object";
+    public static final String ARG_OBJECT = "cat";
 
     @Nullable
     @Override
@@ -28,9 +34,20 @@ public class DemoObjectFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
-        Bundle args = getArguments();
-        ((TextView) view.findViewById(android.R.id.))
-                .setText(Integer.toString(args.getInt(ARG_OBJECT)));
+        List<String> stringList = new ArrayList<String>();
 
+        for(int i = 0; i < 20; i++) {
+            stringList.add("elemento " + i);
+        }
+
+        RecyclerView recyclerView = view.findViewById(R.id.catalogue_view);
+        CatalogueRecyclerViewAdapter catalogRecyclerViewAdapter = new CatalogueRecyclerViewAdapter(stringList, new CatalogueRecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(String s) {
+                Toast.makeText(getActivity(), s, Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView.setAdapter(catalogRecyclerViewAdapter);
     }
 }
