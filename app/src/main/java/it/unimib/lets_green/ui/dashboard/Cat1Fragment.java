4675b6f835450b96cc3b78f0b468aea16d74d5a3
Lorenzo Basavecchia <l.basavecchia@campus.unimib.it>
2021-08-09 17:10:03 +0200

    implementata la navigazione tra le categorie del catalogo con diversi fragment, eliminati file precendenti non più necessari

--- app/src/main/java/it/unimib/lets_green/ui/dashboard/Cat1Fragment.java
+++ app/src/main/java/it/unimib/lets_green/ui/dashboard/Cat1Fragment.java
@@ -11,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,16 +18,13 @@ import java.util.List;
 
 import it.unimib.lets_green.R;
 
-// Instances of this class are fragments representing a single
-// object in our collection.
-public class DemoObjectFragment extends Fragment {
-    public static final String ARG_OBJECT = "cat";
+public class Cat1Fragment extends Fragment {
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_demo_object, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cat1, container, false);
     }
 
     @Override
@@ -40,7 +36,7 @@ public class DemoObjectFragment extends Fragment {
             stringList.add("elemento " + i);
         }
 
-        RecyclerView recyclerView = view.findViewById(R.id.catalogue_view);
+        RecyclerView recyclerView = view.findViewById(R.id.cat1_view);
         CatalogueRecyclerViewAdapter catalogRecyclerViewAdapter = new CatalogueRecyclerViewAdapter(stringList, new CatalogueRecyclerViewAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(String s) {
