073a44e57c1d33f71a7471c4589b12514ce6df98
Federico <f.andreoli4@campus.unimib.it>
2021-08-31 16:17:25 +0200

    aggiornata schermata home

--- app/src/main/java/it/unimib/lets_green/ui/home/HomeFragment.java
+++ app/src/main/java/it/unimib/lets_green/ui/home/HomeFragment.java
@@ -7,31 +7,37 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import it.unimib.lets_green.R;
 
 public class HomeFragment extends Fragment {
-
+    private TextView textCO2emessa;
+    private TextView textCO2assorbita;
     private HomeViewModel homeViewModel;
     private CardView carbonCard;
+    private String CO2emessa;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         carbonCard = root.findViewById(R.id.cardViewPollution);
+        textCO2emessa = root.findViewById(R.id.textView4);
+        textCO2assorbita= root.findViewById(R.id.textView7);
         carbonCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(root).navigate(R.id.pathFragment);
-
             }
         });
+
+        textCO2emessa.setText("1234");
+        textCO2assorbita.setText("123456");
+
         return root;
     }
 }
