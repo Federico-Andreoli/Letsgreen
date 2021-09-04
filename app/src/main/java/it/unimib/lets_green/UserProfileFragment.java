bff2213fec53ef3e6dc04faed66a0dedb78acd6d
Federico <f.andreoli4@campus.unimib.it>
2021-09-04 16:00:19 +0200

    aggiunta persistenza account

--- app/src/main/java/it/unimib/lets_green/UserProfileFragment.java
+++ app/src/main/java/it/unimib/lets_green/UserProfileFragment.java
@@ -1,18 +1,31 @@
 package it.unimib.lets_green;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import it.unimib.lets_green.Firebase.Autentication;
 
 
 public class UserProfileFragment extends Fragment {
+    public Button logOutButton;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
-
+        logOutButton= view.findViewById(R.id.logOut);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Autentication.logOutUser();
+                Toast.makeText(getActivity(), "logOut effettuato", Toast.LENGTH_SHORT).show();
+                Navigation.findNavController(getActivity(), R.id.nav_host_fragment).navigate(R.id.fragment_login);
+            }
+        });
         return view;
     }
    }
