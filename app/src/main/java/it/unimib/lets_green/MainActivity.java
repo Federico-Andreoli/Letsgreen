ebeb60948bff691073f35591b91c109468841371
Marco Boscariol <m.boscariol@campus.unimib.it>
2021-09-06 10:39:41 +0200

    Settato orientamento verticale

--- app/src/main/java/it/unimib/lets_green/MainActivity.java
+++ app/src/main/java/it/unimib/lets_green/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
